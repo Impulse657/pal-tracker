5598f5b61063ecf66cfe5d35327d4a8555229abc
DeMarcus Townsend <dtownsend@kr.af.mil>
2019-08-19 14:09:29 -0600

    Created EnvController to display some env values. Updated WelcomeController to use enviornment variables as well as a constructor

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
-package io.pivotal.pal.tracker.controllers;
+package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    private String welcomeMessage;
+
+    public WelcomeController(@Value("${welcome.message}") String welcomeMessage){
+
+        this.welcomeMessage = welcomeMessage;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello";
+        return welcomeMessage;
     }
 
     @GetMapping("/dumbstuff/{textthing}")
